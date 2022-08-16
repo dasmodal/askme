3b1d88565bcedc453a36015ebee5e0cbd8a31846
Artur Rodkin <das.modal@gmail.com>
2022-08-16 22:54:01 +0700

    add models associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
+  belongs_to :user
+  
   scope :by_recently_created, -> { order(created_at: :desc) }
 
   validates :body, presence: true, length: { maximum: 280 }
