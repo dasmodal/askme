96dea697b136bdc1698952d498063d64aeddecfb
Artur Rodkin <das.modal@gmail.com>
2022-08-17 15:10:48 +0700

    add email index to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_17_072449) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_080557) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_17_072449) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "header_color"
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
