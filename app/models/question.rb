dfba073624a55f694328a25752e62333b650140f
Artur Rodkin <das.modal@gmail.com>
2022-08-18 11:46:43 +0700

    return opportunity to leave questions for unauthorized users

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', optional: true
   
   scope :by_recently_created, -> { order(created_at: :desc) }
 
