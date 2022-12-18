c34742be3f600486d95f854329495b46e8660813
Artur Rodkin <das.modal@gmail.com>
2022-12-18 18:14:41 +0700

    create hashtag and dependent with question

---
+++ app/models/hashtag.rb
@@ -0,0 +1,4 @@
+class Hashtag < ApplicationRecord
+  has_many :hashtag_questions, dependent: :delete_all
+  has_many :questions, through: :hashtag_questions
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
+  has_many :hashtag_questions, dependent: :delete_all
+  has_many :hashtags, through: :hashtag_questions
   
   validates :body, presence: true, length: { maximum: 280 }
 end
