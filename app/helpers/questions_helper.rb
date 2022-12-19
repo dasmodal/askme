c820d93dacdf56bbbd55e7b047151b456e27a408
Artur Rodkin <das.modal@gmail.com>
2022-12-19 11:43:49 +0700

    add helper to questions view for hashtags link

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def hashtags_link(text)
+    text.gsub(/#[[:word:]-]+/) do |htag|
+      htag_param = htag.delete('#').downcase
+      
+      link_to htag, hashtag_path(htag_param)
+    end
+  end
 end
