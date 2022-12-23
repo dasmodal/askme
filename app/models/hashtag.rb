3e50d7fa8d0c3ce87de1e6ebd4be893ccd0a6957
Artur Rodkin <das.modal@gmail.com>
2022-12-23 14:04:54 +0700

    model dependent destroy

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,5 +1,5 @@
 class Hashtag < ApplicationRecord
-  has_many :hashtag_questions, dependent: :delete_all
+  has_many :hashtag_questions, dependent: :destroy
   has_many :questions, through: :hashtag_questions
 
   scope :with_questions, -> { where_exists(:hashtag_questions) }
